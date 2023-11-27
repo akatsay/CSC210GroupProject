2775e0b74f88be19c669592b7d2b327fda4887e3
akatsay <akatsay2@gmail.com>
2023-11-27 15:01:02 -0500

    finish scene switch

--- src/main/java/com/example/grocerystore/ItemsMenu.java
+++ src/main/java/com/example/grocerystore/ItemsMenu.java
@@ -4,38 +4,43 @@ import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class ItemsMenu extends Application {
+public class ItemsMenu extends Pane {
 
     private IceCreamStore iceCreamStore;
-    private String[] itemNames;
     private List<Spinner<Integer>> quantitySpinners;
 
-    public static void main(String[] args) {
-        launch(args);
-    }
-
-    @Override
-    public void start(Stage primaryStage) {
+    public ItemsMenu() {
         iceCreamStore = new IceCreamStore();
-        itemNames = iceCreamStore.getItemNames();
         quantitySpinners = new ArrayList<>();
 
-        primaryStage.setTitle("Ice Cream Store App");
+        // Initialize itemNames here
+        String[] itemNames = iceCreamStore.getItemNames();
 
+        // Use the createItemsMenuPane method to initialize the GridPane
+        GridPane grid = createItemsMenuPane(itemNames);
+
+        getChildren().add(grid);
+    }
+
+    // New method to initialize the GridPane for the ItemsMenu
+    private GridPane createItemsMenuPane(String[] itemNames) {
         GridPane grid = new GridPane();
-        grid.setPadding(new Insets(10, 10, 10, 10));
+        grid.setAlignment(javafx.geometry.Pos.CENTER);
         grid.setVgap(15);
         grid.setHgap(5);
+        grid.setPadding(new Insets(10, 10, 10, 10));
+        grid.getColumnConstraints().add(new ColumnConstraints(200));
 
         // Display menu
-        displayMenu(grid);
-
+        displayMenu(grid, itemNames);
 
         // Button for calculations
         Button calculateButton = new Button("Calculate");
@@ -44,21 +49,19 @@ public class ItemsMenu extends Application {
         // Result display area
         TextArea resultTextArea = new TextArea();
         resultTextArea.setEditable(false);
-        grid.add(resultTextArea, 0, 4, 2, 1);
+        grid.add(resultTextArea, 0, 4, 1, 1);
 
         // Event handling for the calculate button
-        calculateButton.setOnAction(e -> calculateAndDisplay(this.itemNames, resultTextArea));
-
-        Scene scene = new Scene(grid, 400, 300);
-        primaryStage.setScene(scene);
-        primaryStage.show();
+        calculateButton.setOnAction(e -> calculateAndDisplay(itemNames, resultTextArea));
+        return grid;
     }
 
-    private void displayMenu(GridPane grid) {
+    private void displayMenu(GridPane grid, String[] itemNames) {
         // displaying controls and labels for each individual item
-        for (int i = 0; i < this.itemNames.length; i++) {
-            grid.add(new Label(this.itemNames[i]), 0, i);
-            grid.add(new Label("$" + iceCreamStore.getPrice(this.itemNames[i])), 1, i);
+        quantitySpinners = new ArrayList<>();
+        for (int i = 0; i < itemNames.length; i++) {
+            grid.add(new Label(itemNames[i]), 0, i);
+            grid.add(new Label("$" + iceCreamStore.getPrice(itemNames[i])), 1, i);
             Spinner<Integer> quantitySpinner = new Spinner<>(0, 99, 0); // Min, Max, Initial Value
             grid.add(quantitySpinner, 2, i);
 
@@ -68,7 +71,6 @@ public class ItemsMenu extends Application {
     }
 
     private void calculateAndDisplay(String[] itemNames, TextArea resultTextArea) {
-        // Add user validation here
         ArrayList<Integer> quantitiesInt = new ArrayList<>();
 
         // Iterate over the list of Spinners to get their values
@@ -84,3 +86,4 @@ public class ItemsMenu extends Application {
         resultTextArea.setText("Cost before tax: $" + cost + "\nTax: $" + tax + "\nTotal Cost: $" + totalCost);
     }
 }
+
--- src/main/java/com/example/grocerystore/Login.java
+++ src/main/java/com/example/grocerystore/Login.java
@@ -9,64 +9,64 @@ import javafx.scene.control.*;
 
 //Login Application Class
 public class Login extends Application {
-        //Class containing userName and Password validation
-        public boolean isValidUser(String enterUsername, String enterPassword) {
-            IceCreamStore groceryStore = new IceCreamStore();
-            return enterUsername.equals(groceryStore.getUsername()) && enterPassword.equals(groceryStore.getPassword());
-        }
 
-        public static void main(String[] args) {
-            launch(args);
-        }
-        //The stage which is the UI for the login screen
-        @Override
-        public void start(Stage primaryStage) throws Exception {
-            GridPane grid = new GridPane();
-            //Centers the login text box prompt
-            grid.setAlignment(javafx.geometry.Pos.CENTER);
-
-            //Login screen password and username prompt
-            //Username
-            Label usernameLabel = new Label("Username:");
-            grid.add(usernameLabel, 0, 1);
-
-            TextField usernameField = new TextField();
-            grid.add(usernameField, 1, 1);
-
-            //Password
-            Label passwordLabel = new Label("Password:");
-            grid.add(passwordLabel, 0, 2);
-
-            PasswordField passwordField = new PasswordField();
-            grid.add(passwordField, 1, 2);
-
-            //Login button
-            Button loginButton = new Button("Login");
-            grid.add(loginButton, 1, 3);
-
-            final Text actionTarget = new Text();
-            grid.add(actionTarget, 1, 6);
-
-            //Set action which initializes both username and password fields in order to get checked in the if statement
-            loginButton.setOnAction(e -> {
-                String enterUsername = usernameField.getText();
-                String enterPassword = passwordField.getText();
-                // if statement that uses the method in order to check which message it has to display
-                if (isValidUser(enterUsername, enterPassword)) {
-                    actionTarget.setFill(Color.GREEN);
-                    actionTarget.setText("Login successful");
-                }
-                    else {
-                        actionTarget.setFill(Color.RED);
-                        actionTarget.setText("Invalid username or password");
-                    }
-            });
-            //Shows the scene
-            Scene scene = new Scene(grid, 400, 300);
-            primaryStage.setScene(scene);
-            primaryStage.show();
-
-        }
-
+    // Class containing userName and Password validation
+    public boolean isValidUser(String enterUsername, String enterPassword) {
+        IceCreamStore groceryStore = new IceCreamStore();
+        return enterUsername.equals(groceryStore.getUsername()) && enterPassword.equals(groceryStore.getPassword());
     }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("IceCream Store App");
+        GridPane grid = new GridPane();
+        // Centers the login text box prompt
+        grid.setAlignment(javafx.geometry.Pos.CENTER);
+
+        // Login screen password and username prompt
+        // Username
+        Label usernameLabel = new Label("Username:");
+        grid.add(usernameLabel, 0, 1);
+
+        TextField usernameField = new TextField();
+        grid.add(usernameField, 1, 1);
+
+        // Password
+        Label passwordLabel = new Label("Password:");
+        grid.add(passwordLabel, 0, 2);
+
+        PasswordField passwordField = new PasswordField();
+        grid.add(passwordField, 1, 2);
+
+        // Login button
+        Button loginButton = new Button("Login");
+        grid.add(loginButton, 1, 3);
+
+        final Text actionTarget = new Text();
+        grid.add(actionTarget, 1, 6);
+
+        // Set action which initializes both username and password fields in order to get checked in the if statement
+        loginButton.setOnAction(e -> handleLogin(primaryStage, usernameField.getText(), passwordField.getText(), actionTarget));
+
+        // Shows the scene
+        Scene scene = new Scene(grid, 500, 400);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    private void handleLogin(Stage primaryStage, String enterUsername, String enterPassword, Text actionTarget) {
+        if (isValidUser(enterUsername, enterPassword)) {
+            // Switch to ItemsMenu pane upon successful login
+            ItemsMenu itemsMenu = new ItemsMenu();
+            primaryStage.getScene().setRoot(itemsMenu);
+        } else {
+            actionTarget.setFill(Color.RED);
+            actionTarget.setText("Invalid username or password");
+        }
+    }
+}
 
